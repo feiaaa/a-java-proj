d6ccb089aa593d7318d28f9b97ff0eb01b5e936b
feiaaa <1452328755@qq.com>
2021-10-28 10:48:26 +0800

    singleton

--- src/Ruankao/Singleton201411/Configure.java
+++ src/Ruankao/Singleton201411/Configure.java
@@ -2,19 +2,19 @@ package Ruankao.Singleton201411;
 // 此处c++改写为java,config即为singleton
 public class Configure {
     private int data;
-    private static Configure instance;
+    private static Configure instance; // 1,2
     private Configure(){}
     public static Configure getInstance(){
         if(instance==null){
-            instance=new Configure();
+            instance=new Configure(); // 3
         }
-        return instance;
+        return instance;// 4
     }
     public void SetConfigureData(int data){
-        instance.data=data;
+        this.data=data;
     }
     public int GetConfigureData(){
-        return instance.data;
+        return data;
     }
 
 
--- src/Ruankao/Singleton201411/Main.java
+++ src/Ruankao/Singleton201411/Main.java
@@ -3,7 +3,8 @@ package Ruankao.Singleton201411;
 public class Main {
     public static void main(String[] args){
         Configure t=null;
-        t=t.SetConfigureData(1); //
+        t=t.getInstance(); // 5
+        t.SetConfigureData(1);
         int d= t.GetConfigureData();
         System.out.println(d);
     }
